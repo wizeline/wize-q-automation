f91f5a692a09d708810681d1f161de918921f576
Cesar Ivan Guzman Vazquez <cesar.guzman.wizeline@kueski.com>
2022-12-20 17:21:09 -0600

    Delete test files

--- test-1.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://questions-develop.wizeline.com/login');
-  await page.getByRole('button', { name: 'Log in with your Wizeline account' }).click();
-});
